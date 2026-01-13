e100753a5442c8c3d778576f9e18302147fd1026
Роман Емельянов <Emilyen31@yandex.ru>
2026-01-13 19:25:24 +0800

    Добавление функции преобразования даты в форман ДД.ММ.ГГГГ

--- src/widget.py
+++ src/widget.py
@@ -16,3 +16,19 @@ def mask_account_card(number_account_card: str) -> str:
         return f"{user_input[0]} {get_mask_account(user_input[1])}"
     else:
         return "Некорректный ввод"
+
+
+def get_date(iso_date_str):
+    """Функция преобразует дату в формат 'ДД.ММ.ГГГГ' """
+
+
+    # Делим строку с датой '2024-03-11T02:26:18.671407' на две подстроки по символу 'T'
+
+    date_y_m_d = str(iso_date_str.split("T", 1)[0])
+
+    # Делим строку с датой '2024-03-11' на три подстроки по символу '-'
+
+    date_d_m_y = date_y_m_d.split("-", 3)
+
+    # Выводим получившиеся подстроки, начиная с последней, с разделителем "."
+    return f"{date_d_m_y[2]}.{date_d_m_y[1]}.{date_d_m_y[0]}"
